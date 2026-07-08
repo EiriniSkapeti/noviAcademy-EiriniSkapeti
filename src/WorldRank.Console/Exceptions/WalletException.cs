ab057aa4b2ea156b8abe584b8d7ddac923ae31f8
EiriniSkapeti <eiriniskap@gmail.com>
2026-07-08 15:40:48 +0300

    added Exception hierarchy

---
+++ src/WorldRank.Console/Exceptions/WalletException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldRank.Console.Exceptions
+{
+    public class WalletException : Exception
+    {
+        public WalletException() { 
+        
+            public WalletException(string message) : base message {
+        }
+            public WalletException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
